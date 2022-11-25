0a29716b38f81d59fa42a0c5c9f5b17237793ec5
victorialange <viclan2009@hotmail.com>
2022-11-25 04:08:28 -0500

    fixed issue with wrapper in hamburger nav, cleaned up code and added more media queries

--- app.js
+++ app.js
@@ -3,24 +3,26 @@
 
 // grabbing existing HTML elements
 const burgerMenu = document.querySelector(".hamburgerMenu i");
-const navLinks = document.querySelector(".navLinks");
-
+// const slideOutMenu = document.querySelector(".slideOut");
+const slideOutNav = document.querySelector(".hamburgerNav");
+const logo = document.querySelector(".hamburgerNav .logo");
 //add another class to header when user click icon
-const header= document.querySelector("header");
+// const header= document.querySelector("header");
 
 // function
 function toggleNav() {
     burgerMenu.classList.toggle("fa-bars");
     burgerMenu.classList.toggle("fa-times");
-    navLinks.classList.toggle("navActive");
-    header.classList.toggle("headerDown")
+    // slideOutMenu.classList.toggle("menuActive");
+    slideOutNav.classList.toggle("navActive");
+    logo.classList.toggle("noLogo");
+    // header.classList.toggle("headerDown");
 }
 
 // call function after user clicks on icon
 // event listener
 burgerMenu.addEventListener("click", () => {
     toggleNav();
-    
 });
 
 // toggle again when user clicks nav link
@@ -28,8 +30,9 @@ const navLinkOne = document.querySelector(".navLinkOne");
 const navLinkTwo = document.querySelector(".navLinkTwo");
 const navLinkThree = document.querySelector(".navLinkThree");
 const navLinkFour = document.querySelector(".navLinkFour");
+const navLinkFive = document.querySelector(".navLinkFive");
 
-// event listener for a
+// event listener for a tags inside burger menu
 navLinkOne.addEventListener("click", () => {
     toggleNav();
 });
@@ -42,7 +45,9 @@ navLinkThree.addEventListener("click", () => {
 navLinkFour.addEventListener("click", () => {
     toggleNav();
 });
-
+navLinkFive.addEventListener("click", () => {
+    toggleNav();
+});
 
 
 // event listener
