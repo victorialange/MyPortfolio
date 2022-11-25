e4ea9c2ed1e74c393634420734e6207c230c1846
victorialange <viclan2009@hotmail.com>
2022-11-25 09:02:59 -0500

    detached head

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ function toggleNav() {
 
 // call function after user clicks on icon
 // event listener
-burgerMenu.addEventListener("click", () => {
+burgerMenu.addEventListener("click", (event) => {
+    event.preventDefault();
     toggleNav();
 });
 
@@ -33,35 +34,53 @@ const navLinkFour = document.querySelector(".navLinkFour");
 const navLinkFive = document.querySelector(".navLinkFive");
 
 // event listener for a tags inside burger menu
-navLinkOne.addEventListener("click", () => {
+navLinkOne.addEventListener("click", (event) => {
+  
     toggleNav();
 });
-navLinkTwo.addEventListener("click", () => {
+navLinkTwo.addEventListener("click", (event) => {
+   
     toggleNav();
 });
-navLinkThree.addEventListener("click", () => {
+navLinkThree.addEventListener("click", (event) => {
+ 
     toggleNav();
 });
-navLinkFour.addEventListener("click", () => {
+navLinkFour.addEventListener("click", (event) => {
+    
     toggleNav();
 });
-navLinkFive.addEventListener("click", () => {
+navLinkFive.addEventListener("click", (event) => {
+
     toggleNav();
 });
 
+// clearing the form after clicking go back when submitting via formspree
+// information taken from formspree help: https://help.formspree.io/hc/en-us/articles/1500009404742-How-to-clear-a-form-after-submission
+
+window.onbeforeunload = () => {
+    for(const form of document.getElementsByTagName('form')) {
+      form.reset();
+    }
+}
+
+// this will unfortunately not work because this is causing the form to clear its input before it is being processed/handled by formspree
 
 // event listener for clearing input once form submitted
-const formEl = document.querySelector("form");
-const inputName = document.querySelector("input[name=userName]");
-const inputEmail = document.querySelector("input[name=userEmail]");
-const textArea = document.querySelector("textarea");
+// const formEl = document.querySelector("form");
+// const inputName = document.querySelector("input[name=userName]");
+// const inputEmail = document.querySelector("input[name=userEmail]");
+// const textArea = document.querySelector("textarea");
 
 
-formEl.addEventListener("submit", (event) => {
-    event.preventDefault();
-    inputName.value = "";
-    inputEmail.value = "";
-    textArea.value = "";
-})
+// formEl.addEventListener("submit", (event) => {
+//     // event.preventDefault();
+//     if (inputName.value && inputEmail.value && textArea.value) {
+//        inputName.value = ""; 
+//        inputEmail.value = ""; 
+//        textArea.value = "";
+//     }
+
+// })
 
 
